0e08d6eebe3a78ecff30544f7e29aa85c67ea988
msoto <marcoengnr@gmail.com>
2026-03-03 16:43:44 +0000

    LW updating naming on apiv9

--- OcrLibrary/BreakerTableParserAPIv9.py
+++ OcrLibrary/BreakerTableParserAPIv9.py
@@ -1,4 +1,4 @@
-# OcrLibrary/BreakerTableParserAPIv8.py
+# OcrLibrary/BreakerTableParserAPIv9.py
 import sys, os, inspect
 import re
 import cv2
@@ -10,7 +10,7 @@ _REPO_ROOT  = os.path.dirname(_OCRLIB_DIR)
 if _REPO_ROOT not in sys.path:
     sys.path.insert(0, _REPO_ROOT)
 
-API_VERSION = "API_8"
+API_VERSION = "API_9"
 API_ORIGIN  = __file__
 
 SNAP_MAP = {
